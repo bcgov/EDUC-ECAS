38256a3f4c4e07bcaeee3d08042b8488917edeab
Dirk <dirkbritton@gmail.com>
2019-04-08 12:37:30 -0700

    dynamics connection tests

--- web-app/app/Http/Controllers/DashboardController.php
+++ web-app/app/Http/Controllers/DashboardController.php
@@ -68,4 +68,41 @@ class DashboardController extends Controller
     {
         return $request->all();
     }
+
+    public function test1()
+    {
+        $client = \AlexaCRM\WebAPI\ClientFactory::createOnlineClient(
+            'http://ecas-pvpywj-dev.pathfinder.gov.bc.ca/',
+            '96337d02-539f-4982-a89a-c1d25c78bd3b',
+            'NltSOQMgJ2DwVbjw+6hpceiyGzGYWp6mDI6v5smQX+k='
+        );
+    }
+
+    public function signIn()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        // Initialize the OAuth client
+        $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+            'clientId'                => env('OAUTH_APP_ID'),
+            'clientSecret'            => env('OAUTH_APP_PASSWORD'),
+            'redirectUri'             => env('OAUTH_REDIRECT_URI'),
+            'urlAuthorize'            => env('OAUTH_AUTHORITY').env('OAUTH_AUTHORIZE_ENDPOINT'),
+            'urlAccessToken'          => env('OAUTH_AUTHORITY').env('OAUTH_TOKEN_ENDPOINT'),
+            'urlResourceOwnerDetails' => '',
+            'scopes'                  => env('OAUTH_SCOPES')
+        ]);
+
+        // Generate the auth URL
+        $authorizationUrl = $oauthClient->getAuthorizationUrl();
+
+        // Save client state so we can validate in response
+        $_SESSION['oauth_state'] = $oauthClient->getState();
+
+        // Redirect to authorization endpoint
+        header('Location: '.$authorizationUrl);
+        exit();
+    }
 }
