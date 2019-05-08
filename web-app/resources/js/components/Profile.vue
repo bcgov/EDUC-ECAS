989463cb306caa1c1d7802815ee0dbcaaaf76a38
Dirk <dirkbritton@gmail.com>
2019-05-08 10:40:26 -0700

    Required Inputs

--- web-app/resources/js/components/Profile.vue
+++ web-app/resources/js/components/Profile.vue
@@ -11,13 +11,13 @@
                                name="preferred_first_name" id="preferred_first_name">
                     </div>
                     <div class="form-group col">
-                        <label for="first_name">First Name</label>
+                        <label for="first_name" class="required">First Name</label>
                         <input v-model="user_local.first_name" type="text" class="form-control" name="first_name"
                                id="first_name">
                         <form-error :errors="errors" field="first_name"></form-error>
                     </div>
                     <div class="form-group col">
-                        <label for="last_name">Last Name</label>
+                        <label for="last_name" class="required">Last Name</label>
                         <input v-model="user_local.last_name" type="text" class="form-control" name="last_name"
                                id="last_name">
                         <form-error :errors="errors" field="last_name"></form-error>
@@ -25,7 +25,7 @@
                 </div>
                 <div class="form-row">
                     <div class="form-group col">
-                        <label for="email">email</label>
+                        <label for="email" class="required">email</label>
                         <input v-model="user_local.email"
                                type="email"
                                class="form-control"
@@ -35,7 +35,7 @@
                         <form-error :errors="errors" field="email"></form-error>
                     </div>
                     <div class="form-group col">
-                        <label for="phone">Phone</label>
+                        <label for="phone" class="required">Phone</label>
                         <input v-model="user_local.phone"
                                type="tel"
                                class="form-control"
@@ -52,7 +52,7 @@
                 <div class="form-row">
                     <div class="col">
                         <div class="form-group">
-                            <label for="address_1">Home Address</label>
+                            <label for="address_1" class="required">Home Address</label>
                             <input v-model="user_local.address_1" type="text" class="form-control" name="address_1"
                                    id="address_1">
                             <form-error :errors="errors" field="address_1"></form-error>
@@ -65,7 +65,7 @@
                     </div>
                     <div class="col">
                         <div class="form-group">
-                            <label for="city">City</label>
+                            <label for="city" class="required">City</label>
                             <input v-model="user_local.city" type="text" class="form-control" name="city" id="city">
                             <form-error :errors="errors" field="city"></form-error>
                         </div>
@@ -77,7 +77,7 @@
                                 </select>
                             </div>
                             <div class="form-group col">
-                                <label for="postal_code">Postal Code</label>
+                                <label for="postal_code" class="required">Postal Code</label>
                                 <input v-model="user_local.postal_code" type="text" class="form-control" name="postal_code"
                                        id="postal_code">
                                 <form-error :errors="errors" field="postal_code"></form-error>
@@ -127,6 +127,9 @@
                     <div class="col">
                         <button class="btn btn-primary btn-block" v-on:click.prevent="saveProfile" dusk="save">Save</button>
                     </div>
+                </div>
+                <div class="row">
+                    <div class="col">* Required</div>
                 </div>
             </form>
         </div>
@@ -231,5 +234,5 @@
 </script>
 
 <style scoped>
-
+    .required:after { content:" *"; }
 </style>
