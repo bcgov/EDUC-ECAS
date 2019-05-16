60273cf18e6a23b39abfca6c86c3208532420736
Dirk <dirkbritton@gmail.com>
2019-05-16 10:47:21 -0700

    api test

--- web-app/routes/web.php
+++ web-app/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/contact', 'PageController@contact');
 
 // App Specific Routes
 
-Route::get('/apitest', 'DashBoardController@apitest');
+Route::get('/apitest', 'DashboardController@apitest');
 
 Route::get('/', 'DashboardController@login');
 Route::post('/login', 'DashboardController@postLogin');
