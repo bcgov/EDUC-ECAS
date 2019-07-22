8c400d5499a4bf0b813ea8504f84d405d2cc7313
Jonathan Longe <jlonge@corebox.net>
2019-07-22 11:41:08 -0700

    add Keycloak Guard

--- web-app/config/auth.php
+++ web-app/config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'keycloak',
             'provider' => 'users',
             'hash' => false,
         ],
