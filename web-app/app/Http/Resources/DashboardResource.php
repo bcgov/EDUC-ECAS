65791f4bcef4a4bcf30b52c0f1fc2d7bfd2a1ec8
Jonathan Longe <jlonge@corebox.net>
2019-07-25 14:38:12 -0700

    remove personal data from DashboardResource

--- web-app/app/Http/Resources/DashboardResource.php
+++ web-app/app/Http/Resources/DashboardResource.php
@@ -19,21 +19,16 @@ class DashboardResource extends JsonResource
 
         $repository             = env('DATASET') == 'Dynamics' ? 'Dynamics' : 'MockEntities\Repository';
         $sessions               = ( new CacheDecorator(App::make('App\\' . $repository .'\Session')))->all();
-        $profile                = ( new CacheDecorator(App::make('App\\' . $repository .'\Profile')))->get($this->id);
-        $profile_credentials    = ( new CacheDecorator(App::make('App\\' . $repository .'\ProfileCredential')))->filter(['user_id'=> $this->id]);
-        $assignments            = ( new CacheDecorator(App::make('App\\' . $repository .'\Assignment')))->filter(['user_id'=> $this->id]);
         $districts              = ( new CacheDecorator(App::make('App\\' . $repository .'\District')))->all();
         $credentials            = ( new CacheDecorator(App::make('App\\' . $repository .'\Credential')))->all();
         $regions                = ( new CacheDecorator(App::make('App\\' . $repository .'\Region')))->all();
         $schools                = ( new CacheDecorator(App::make('App\\' . $repository .'\School')))->all();
+        $subjects               = ( new CacheDecorator(App::make('App\\' . $repository .'\Subject')))->all();
 
 
         return [
-            'user'                  => new ProfileResource($profile),
-            'user_credentials'      => ProfileCredentialResource::collection($profile_credentials),
-            'assignments'           => AssignmentResource::collection($assignments),
             'sessions'              => SessionResource::collection($sessions),
-            'subjects'              => ( new CacheDecorator(App::make('App\\' . $repository .'\Subject')))->all(),
+            'subjects'              => SimpleResource::collection($subjects),
             'districts'             => SimpleResource::collection($districts),
             'regions'               => SimpleResource::collection($regions),
             'credentials'           => SimpleResource::collection($credentials),
