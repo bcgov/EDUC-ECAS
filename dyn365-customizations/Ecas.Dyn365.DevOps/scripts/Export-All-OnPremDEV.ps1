a0719276eeda2592cf8eb0242599f85dad7541f5
Greg <45373249+GregFPoirier@users.noreply.github.com>
2019-11-26 11:26:48 -0800

    Updated for Nov 26th UAT Release.

--- dyn365-customizations/Ecas.Dyn365.DevOps/scripts/Export-All-OnPremDEV.ps1
+++ dyn365-customizations/Ecas.Dyn365.DevOps/scripts/Export-All-OnPremDEV.ps1
@@ -1,6 +1,6 @@
 ﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-[Environment]::SetEnvironmentVariable("CRM_SDK_PATH", "F:\CRM-SDK-v9\Tools")
+[Environment]::SetEnvironmentVariable("CRM_SDK_PATH", "H:\CRM-SDK-v9\Tools")
 Import-Module .\Microsoft.Xrm.Data.Powershell\2.8.0\Microsoft.Xrm.Data.PowerShell.psd1
 Import-Module .\Adoxio.Dynamics.DevOps\0.8.0\Adoxio.Dynamics.DevOps.psd1
 .\Export.ps1 -CrmConnectionName OnpremiseDEV-ECAS -ExportSettings ECASCustomizations -Actions Solutions
