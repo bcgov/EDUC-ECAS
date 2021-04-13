ee503952367aee3a3378f2e9c48cb0b2e971a4ed
Jinil Sung <infstar@gmail.com>
2021-04-13 11:34:00 -0700

    FileUploadedList & FileDownloader components are newly added.
    FileUploadedList & FileDownloader components are newly added.

---
+++ web-app/resources/js/components/FileDownloader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <button class="btn btn-primary btn-sm float-right" v-on:click="closeModal">X</button>
+            <h2>Download file</h2>
+        </div>
+        <div class="card-body">
+            <div class="file-download-div">
+                <div class="download mx-2 my-2">
+                    <span class="pl-2"><a href="#">Jenni McConnell - E21-12345</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FileDownloader",
+  data() {
+    return {
+      files: [],
+    }
+  },
+  methods: {
+    closeModal() {
+        this.$modal.hide('file_download_form');
+    },
+  }
+}
+</script>
+
+<style scoped>
+.download {
+  border: 1px #ff0000 dotted;
+}
+</style>
---
+++ web-app/resources/js/components/FileUploadedList.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <button class="btn btn-primary btn-sm float-right" v-on:click="closeModal">X</button>
+            <h2>Uploaded files</h2>
+        </div>
+        <div class="card-body">
+            <table class="table table-sm table-borderless my-2">
+                <thead>
+                    <tr>
+                        <th scope="col">File name</th>
+                        <th scope="col">Size</th>
+                        <th scope="col">&nbsp;</th>
+                        <th scope="col">&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Untitled.pdf</td>
+                        <td>205 KB</td>
+                        <td>&nbsp;</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-danger">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Untitled2.pdf</td>
+                        <td>312 KB</td>
+                        <td>&nbsp;</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-danger">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FileUploadedList",
+  data() {
+    return {
+      files: [],
+    }
+  },
+  methods: {
+    closeModal() {
+        this.$modal.hide('file_uploaded_list');
+    },
+  }
+}
+</script>
