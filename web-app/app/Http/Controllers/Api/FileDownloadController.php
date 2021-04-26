b0dffd371b80403075f3b0613a341956cf279f9e
Burak Akcen <28536163+burock7@users.noreply.github.com>
2021-04-26 16:54:26 -0700

    Update FileDownloadController.php

--- web-app/app/Http/Controllers/Api/FileDownloadController.php
+++ web-app/app/Http/Controllers/Api/FileDownloadController.php
@@ -74,7 +74,6 @@ class FileDownloadController extends Controller
         header('Content-Description: File Transfer');
         header('Content-Disposition: attachment; filename='.$filename);
         header('Content-Transfer-Encoding: binary');
-        header('Content-Type: application/pdf');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
