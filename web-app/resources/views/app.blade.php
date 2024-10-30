938350078827ed258b3da5a1bc2e3d4d925d479d
Khaled Smaoui <khaled.smaoui@gov.bc.ca>
2024-10-30 14:18:15 -0700

    Fixed typo error with style class name

--- web-app/resources/views/app.blade.php
+++ web-app/resources/views/app.blade.php
@@ -23,7 +23,7 @@
             </div>
             <div class="col text-center text-light header-title">
                 <h3 class="header-title-desktop" >Electronic Contract Administration System (ECAS)</h3>
-				<h3 class="eader-title-mobile" >ECAS</h3>
+				<h3 class="header-title-mobile" >ECAS</h3>
             </div>
             <div class="col-1"></div>
         </div>
