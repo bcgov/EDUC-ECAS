0d4106670ee4dfb5e6602d68a4040f202802b73f
Khaled <45347802+JavaDeveloper456788@users.noreply.github.com>
2024-11-21 16:26:20 -0800

    Update override-configmap-dev.sh

--- openshift/override-configmap-dev.sh
+++ openshift/override-configmap-dev.sh
@@ -41,25 +41,13 @@ PARSER_CONFIG="
 ###########################################################
 echo Creating config map "$APP_NAME"-config-map
 oc create -n "$NAMESPACE" configmap "$APP_NAME"-config-map \
-  --from-literal=APP_LOG_LEVEL="$APP_LOG_LEVEL" \
-  --from-literal=CONNECTION_TIMEOUT="60000" \
-  --from-literal=EDUC_SCHOOL_API="http://school-api-master.$COMMON_NAMESPACE-$envValue.svc.cluster.local:8080/" \
-  --from-literal=ENABLE_FLYWAY="true" \
-  --from-literal=ENABLE_SPLUNK_LOG_HELPER="false" \
-  --from-literal=ENABLE_STUDENT_ID_PEN_XREF="true" \
-  --from-literal=ENABLE_TRAX_UPDATE="true" \
-  --from-literal=GRAD_GRADUATION_REPORT_API="http://educ-grad-graduation-report-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/" \
-  --from-literal=GRAD_PROGRAM_API="http://educ-grad-program-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/" \
-  --from-literal=GRAD_STUDENT_GRADUATION_API="http://educ-grad-student-graduation-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/" \
-  --from-literal=GRAD_TRAX_API="http://educ-grad-trax-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/"\
-  --from-literal=MAXIMUM_POOL_SIZE="20" \
-  --from-literal=MAX_RETRY_ATTEMPTS="3" \
-  --from-literal=PEN_API="http://student-api-master.$COMMON_NAMESPACE-$envValue.svc.cluster.local:8080/" \
-  --from-literal=CRON_SCHEDULED_REFRESH_NON_GRAD_STATUS="0 0 0 1 * ?" \
+  --from-literal=APP_DEBUG="$APP_DEBUG" \
+  --from-literal=DOCUMENTROOT="$DOCUMENTROOT" \
+  --from-literal=DYNAMICSBASEURL="$DYNAMICSBASEURL" \
   --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
-oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+oc create -n "$NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
   --from-literal=parsers.conf="$PARSER_CONFIG" \
   --dry-run=client -o yaml | oc apply -f -
