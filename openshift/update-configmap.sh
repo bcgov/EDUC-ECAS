ef2a73a1917ad2d9f59ccdd88b4599b84072fdf8
cditcher <chris.ditcher@gov.bc.ca>
2024-11-22 10:30:26 -0800

    Minor refactoring

--- openshift/update-configmap.sh
+++ openshift/update-configmap.sh
@@ -7,44 +7,6 @@ APP_DEBUG=$3
 DOCUMENTROOT=$4
 DYNAMICSBASEURL=$5
 
-SPLUNK_URL="gww.splunk.educ.gov.bc.ca"
-FLB_CONFIG="[SERVICE]
-   Flush        1
-   Daemon       Off
-   Log_Level    info
-   HTTP_Server   On
-   HTTP_Listen   0.0.0.0
-   Parsers_File parsers.conf
-[INPUT]
-   Name   tail
-   Path   /mnt/log/*
-   Exclude_Path *.gz,*.zip
-   Parser docker
-   Mem_Buf_Limit 20MB
-   Buffer_Max_Size 1MB
-[FILTER]
-   Name record_modifier
-   Match *
-   Record hostname \${HOSTNAME}
-[OUTPUT]
-   Name   stdout
-   Match  absolutely_nothing_bud
-   Log_Level    off
-[OUTPUT]
-   Name  splunk
-   Match *
-   Host  $SPLUNK_URL
-   Port  443
-   TLS         On
-   TLS.Verify  Off
-   Message_Key $APP_NAME
-   Splunk_Token $SPLUNK_TOKEN
-"
-PARSER_CONFIG="
-[PARSER]
-    Name        docker
-    Format      json
-"
 ###########################################################
 #Setup for config-maps
 ###########################################################
@@ -52,8 +14,7 @@ echo Creating config map "$APP_NAME"-config-map
 oc create -n "$NAMESPACE" configmap "$APP_NAME"-config-map \
   --from-literal=APP_DEBUG="$APP_DEBUG" \
   --from-literal=DOCUMENTROOT="$DOCUMENTROOT" \
- --from-literal=DYNAMICSBASEURL="$DYNAMICSBASEURL" \
-
+  --from-literal=DYNAMICSBASEURL="$DYNAMICSBASEURL" \
   --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
